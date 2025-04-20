726ec1f4c2bbcf1cbdccfeb622ebdea53fb4ed4c
Ildar Murtazin <1919mur@gmail.com>
2025-04-20 08:20:26 +0300

    Добавлена работа батчами.

--- lib/actions/start_word_action.rb
+++ lib/actions/start_word_action.rb
@@ -2,14 +2,17 @@ class Actions::StartWordAction < Actions::BaseAction
   attr_reader :result
 
   def initial
-    @result = WordService.find_word(session[:user_authorize][params.user_id][:user])
+    if session[:word_batch].nil? || session[:word_batch].empty?
+      fetch_new_word_batch
+    end
+
+    @result = get_next_word_from_batch
 
     on_redirect do
       available_actions = [
         [{ text: "Знаю слово", callback_data: "yes" },
-        { text: "Не знаю слово", callback_data: "no" }]
+         { text: "Не знаю слово", callback_data: "no" }]
       ]
-
       client.do_send_message(text: handle_word, parse_mode: "MarkdownV2")
       client.do_send_actions(text: "Выберите действие:", actions: available_actions)
     end
@@ -27,17 +30,46 @@ class Actions::StartWordAction < Actions::BaseAction
 
   private
 
+  def fetch_new_word_batch
+    user = session[:user_authorize][params.user_id][:user]
+    batch_result = WordService.find_word_batch(user, 20)
+
+    if batch_result[:status]
+      session[:word_batch] = batch_result[:object].map do |word|
+        { word: word, used: false }
+      end
+    else
+      session[:word_batch] = []
+    end
+  end
+
+  def get_next_word_from_batch
+    unused_word = session[:word_batch].find { |w| !w[:used] }
+
+    if unused_word
+      unused_word[:used] = true
+      { status: true, object: unused_word[:word] }
+    else
+      fetch_new_word_batch
+
+      if session[:word_batch].any?
+        get_next_word_from_batch
+      else
+        { status: false, object: "Доступных слов нет" }
+      end
+    end
+  end
+
   def handle_word
     if @result[:status]
       text
     else
       warning_text
-      test
     end
   end
 
   def text
-    <<-EOL
+    <<~EOL
       Слово: *#{@result[:object].word.capitalize}*
       Транскрипция: `#{@result[:object].transcription.capitalize}`
       Перевод: ||#{@result[:object].translation.capitalize}||
@@ -45,12 +77,8 @@ class Actions::StartWordAction < Actions::BaseAction
   end
 
   def warning_text
-    <<-EOL
+    <<~EOL
       Доступных слов не осталось
     EOL
-  end
-
-  def test
-    redirect_to Actions::ListAction
   end
 end
--- lib/word_service.rb
+++ lib/word_service.rb
@@ -33,7 +33,7 @@ class WordService
     def find_word(user)
       user_word_relation = WordProgres
         .where(user_id: user.id, is_difficult: false)
-        .where { next_appearance_at <= Time.now }
+        .where(Sequel.lit('next_appearance_at <= ?', Time.now))
         .order(Sequel.asc(:next_appearance_at))
         .first
 
@@ -49,20 +49,54 @@ class WordService
       end
     end
 
+    def find_word_batch(user, count = 20)
+      review_words = WordProgres
+        .where(user_id: user.id, is_difficult: false)
+        .where(Sequel.lit('next_appearance_at <= ?', Time.now))
+        .order(Sequel.asc(:next_appearance_at))
+        .limit(count)
+        .all
+        .map { |wp| wp.word }
+
+      if review_words.length >= count
+        return { status: true, object: review_words.uniq.take(count) }
+      end
+
+      remaining_count = count - review_words.length
+      new_words_result = find_words_for_new_relations(user, remaining_count)
+
+      if new_words_result[:status]
+        all_words = review_words + new_words_result[:object]
+        { status: true, object: all_words.uniq }
+      else
+        if review_words.any?
+          { status: true, object: review_words.uniq }
+        else
+          { status: false, object: "Доступных слов нет" }
+        end
+      end
+    end
+
     private
 
-    def find_word_for_new_relations(user)
+    def find_words_for_new_relations(user, count = 20)
       used_word_ids = WordProgres.where(user_id: user.id).select(:word_id)
-      random_word = Word.where { id !~ used_word_ids }.order(Sequel.lit('RANDOM()')).first
 
-      if random_word
-        user_word = WordProgres.create(
-          user_id: user.id,
-          word_id: random_word.id,
-          reviewed_at: Time.now,
-          next_appearance_at: Time.now
-        )
-        { status: true, object: random_word }
+      available_words = Word.exclude(id: used_word_ids).all
+
+      if available_words.any?
+        random_words = available_words.shuffle.take(count)
+
+        random_words.each do |word|
+          WordProgres.create(
+            user_id: user.id,
+            word_id: word.id,
+            reviewed_at: Time.now,
+            next_appearance_at: Time.now
+          )
+        end
+
+        { status: true, object: random_words }
       else
         { status: false, object: "Нет доступных слов для создания связи" }
       end
